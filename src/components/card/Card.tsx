00749764eaefed7d9f98193a6b75a1d345fad8e7
Mateo Ossa <trevorsoul.ts@gmail.com>
2022-12-31 11:37:50 -0300

    govers and resize working

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -12,13 +12,13 @@ const card: React.FC = () => {
         </div>
     */
     return (
-        <div className='w-full h-[calc(100%-2.5rem)] sm:h-[calc(100%-3.5rem)] lg:h-[calc(100%-5rem)] rounded-2xl grid grid-rows-[3fr_1fr] lg:grid-rows-[8fr_3fr] overflow-hidden'>
+        <div className='group/edit w-full lg:w-4/5 lg:hover:w-full m-auto  h-[calc(100%-2.5rem)] sm:h-[calc(100%-3.5rem)] lg:h-[calc(90%-5rem)] lg:hover:h-[calc(100%-5rem)] rounded-2xl grid grid-rows-[3fr_1fr] lg:grid-rows-[8fr_3fr] overflow-hidden'>
             <img
                 src={Image}
                 alt='GameName'
                 className='w-full h-full overflow-hidden object-cover object-[50%_10%]'
             ></img>
-            <div className='w-full h-full md:p-5 lg:hover:grid lg:grid-cols-[6fr_5fr] bg-background-card'>
+            <div className='lg:group-hover/edit:grid w-full h-full lg:group-hover/edit:p-5 lg:grid-cols-[6fr_5fr] bg-background-card'>
                 <div className='w-full h-full bg-yellow-500 flex justify-center items-center'></div>
                 <div className='w-full bg-orange-600 flex justify-center items-center'></div>
             </div>
