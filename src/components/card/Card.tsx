6b605caa06c85f531e7edfe481baa617fe429c83
Mateo Ossa <trevorsoul.ts@gmail.com>
2023-01-01 08:59:47 -0300

    done:[ increased card scale ] due:[ change color scheme ]

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,21 +2,9 @@ import React from 'react';
 import Image from '../../assets/card-Images/tetris.png';
 
 const card: React.FC = () => {
-    /* <div className='w-full h-full flex-col flex justify-start items-start px-5 my-auto '>
-                        <h1>Game Name</h1>
-                        <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing
-                            elit. Facere sit eos minus quos natus accusantium
-                            explicabo odit velit dolorem esse!
-                        </p>
-                        
-                    </div>
-                    <div className='w-full lg:transform lg:scale-x-0 lg:group-hover/edit:scale-x-100 lg:transition-all delay-75 lg:duration-500 bg-orange-600 flex justify-center items-center'>
-
-                    </div> */
     return (
         <div className='relative flex justify-center items-center w-full h-[calc(100%-2.5rem)] sm:h-[calc(100%-3.5rem)] lg:h-[calc(100%-5rem)] lg:py-8'>
-            <div className='group/edit lg:absolute top-1/2 -translate-y-1/2 w-full lg:w-3/4 lg:hover:scale-x-105  h-full lg:h-5/6 lg:m-auto lg:hover:scale-y-105 rounded-md border border-background-primaryButton border-opacity-50 lg:hover:border-0 lg:shadow-none lg:hover:shadow-card grid grid-rows-[3fr_1fr] lg:grid-rows-[8fr_3fr] lg:hover:grid-rows-[8fr_4fr] lg:transition-all delay-75 lg:duration-500  overflow-hidden'>
+            <div className='group/edit lg:absolute top-1/2 -translate-y-[47.5%] w-full lg:w-3/4 lg:hover:scale-x-[130%]  h-full lg:h-5/6 lg:m-auto lg:hover:scale-y-[122.5%] rounded-md border border-background-primaryButton border-opacity-50 lg:hover:border-0 lg:shadow-none lg:hover:shadow-card grid grid-rows-[3fr_1fr] lg:grid-rows-[8fr_3fr] lg:hover:grid-rows-[8fr_4fr] lg:transition-all delay-75 lg:duration-500  overflow-hidden'>
                 <img
                     src={Image}
                     alt='GameName'
