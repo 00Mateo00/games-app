a9f29e694b660943a727c234f5c5b98b7c47662a
Mateo Ossa <trevorsoul.ts@gmail.com>
2023-01-03 07:36:15 -0300

    com back here

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Image from '../../assets/card-Images/tetris.png';
 
 const Card: React.FC = () => {
-    const primary =
-        'bg-background-primaryButton rounded-xl w-10 sm:w-20 h-6 sm:h-7 flex items-center justify-center';
+    /* grid grid-cols-[6fr_0fr] transition-all delay-75 duration-500 w-full h-full bg-purple-900 z-[1] relative before:absolute before:opacity-0 before:content-[""] before:top-0 before:left-0 before:z-[-1] before:bg-gradient-to-br before:from-violet-600 before:to-violet-800 before:transition-all before:delay-75 before:duration-500 lg:group-hover/edit:before:opacity-100 before:w-full before:h-full  */
+
     const cardContent = (
         <>
             <img
@@ -11,26 +11,36 @@ const Card: React.FC = () => {
                 alt='GameName'
                 className='w-full h-full overflow-hidden object-cover object-[50%_10%]'
             ></img>
-            <div className='lg:childButton:truncate lg:childButton:scale-y-0 lg:group-hover/edit:childButton:scale-y-100  lg:childButton:transition-all  lg:childButton:delay-0 lg:childButton:duration-100 lg:group-hover/edit:childButton:delay-[575ms] lg:group-hover/edit:childButton:duration-100 lg:grid grid-cols-[6fr_0fr] lg:group-hover/edit:grid-cols-[6fr_5fr] lg:transition-all lg:delay-75 lg:duration-500 w-full h-full lg:group-hover/edit:p-5 bg-purple-900 z-[1] relative before:absolute before:opacity-0 before:content-[""] before:top-0 before:left-0 before:z-[-1] before:bg-gradient-to-br before:from-violet-600 before:to-violet-800 before:transition-all  before:delay-75 before:duration-500 lg:group-hover/edit:before:opacity-100 before:w-full before:h-full '>
-                <div className='w-full h-full flex-col flex justify-start items-start px-5 my-auto '>
-                    <h1>Game Name</h1>
-                    <p>
+            <div className='grid grid-cols-[6fr_0fr] transition-all delay-75 duration-500 w-full h-full bg-purple-900 z-[1] relative before:absolute before:opacity-0 before:content-[""] before:top-0 before:left-0 before:z-[-1] before:bg-gradient-to-br before:from-violet-600 before:to-violet-800 before:transition-all before:delay-75 before:duration-500 lg:group-hover/edit:before:opacity-100 before:w-full before:h-full '>
+                <div className='w-full h-full grid grid-row-[1fr_0fr_0fr] group-hover/edit:grid-row-[1fr_1fr_0fr]'>
+                    <h1 className='w-full h-full flex justify-center items-center bg-red-500 transition-all'>
+                        Game Name
+                    </h1>
+                    <p className='truncate scale-0 w-0 h-0 group-hover/edit:w-full group-hover/edit:h-full group-hover/edit:scale-100 transition-all'>
                         Lorem ipsum dolor, sit amet consectetur adipisicing
                         elit. Facere sit eos minus quos natus accusantium
                         explicabo odit velit dolorem esse!
                     </p>
-                    <button className={primary}>PLAY</button>
+                    <button className='bg-background-primaryButton rounded-xl w-0 h-0 flex items-center justify-center truncate scale-y-0 transition-all'>
+                        PLAY
+                    </button>
                 </div>
-                <div className='w-full lg:scale-x-0 lg:group-hover/edit:scale-x-100  lg:transition-all delay-75 lg:duration-500 bg-orange-400 flex flex-col justify-center items-center scale-y-0 truncate rounded-md transition-all duration-500 group-hover/edit:scale-y-100 '>
+                <div className='w-full scale-0 transition-all delay-75 duration-500 bg-orange-400 flex flex-col justify-center items-center truncate rounded-md'>
                     <div className='flex justify-around items-center w-full h-full'>
-                        <button>easy</button>
-                        <button>medium</button>
-                        <button>hard</button>
+                        <button className='truncate'>easy</button>
+                        <button className='truncate'>medium</button>
+                        <button className='truncate'>hard</button>
                     </div>
                     <div className='flex justify-around items-center w-full h-full'>
-                        <button className={primary}>settiings</button>
-                        <button className={primary}>scores</button>
-                        <button className={primary}>tutorial</button>
+                        <button className='bg-background-primaryButton rounded-xl w-10 sm:w-20 h-6 sm:h-7 flex items-center justify-center truncate'>
+                            settiings
+                        </button>
+                        <button className='bg-background-primaryButton rounded-xl w-10 sm:w-20 h-6 sm:h-7 flex items-center justify-center truncate'>
+                            scores
+                        </button>
+                        <button className='bg-background-primaryButton rounded-xl w-10 sm:w-20 h-6 sm:h-7 flex items-center justify-center truncate'>
+                            tutorial
+                        </button>
                     </div>
                 </div>
             </div>
