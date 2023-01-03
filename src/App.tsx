6a44da463b375338915998d5965d37bae551eed9
Mateo Ossa <trevorsoul.ts@gmail.com>
2023-01-03 19:21:51 -0300

    layout working+

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ function App(): JSX.Element {
     return (
         <div className='w-screen h-screen flex justify-center items-center bg-neutral-900 relative overflow-hidden'>
             <header className='absolute top-0 h-11 w-full bg-purple-900'></header>
-            <div className='h-full w-1/2 bg-violet-500 py-11'>
-                <div className='h-full w-full overflow-auto bg-slate-500 p-20'>
-                    <div className='bg-blue-500 w-full h-11'></div>
-                    <div className='grid auto-rows-[23rem] grid-cols-3 gap-3 p-5'>
+            <div className='h-full w-1/2 bg-violet-500 pt-11'>
+                <div className='h-full w-full overflow-auto bg-slate-500'>
+                    <div className='flex h-11 w-full items-center justify-around bg-blue-500 px-16'>
+                        <div className='h-full w-full bg-neutral-400'></div>
+                    </div>
+                    <div className='grid auto-rows-[23rem] grid-cols-3 gap-3 py-2 px-20'>
                         <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
                         <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
                         <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
@@ -26,8 +28,8 @@ function App(): JSX.Element {
                         <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
                         <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
                     </div>
+                    <footer className='h-12 w-full bg-orange-500'></footer>
                 </div>
-                <footer className='h-11 w-full bg-orange-500'></footer>
             </div>
         </div>
     );
