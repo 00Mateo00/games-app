ccd2744ad5c743d45add7e20ebf714c4dc1354f5
Mateo Ossa <trevorsoul.ts@gmail.com>
2023-01-03 17:27:54 -0300

    the layout works

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import './App.scss';
-import Main from './components/main/Main';
 
 function App(): JSX.Element {
     return (
         <div className='w-screen h-screen flex justify-center items-center bg-neutral-900 relative overflow-hidden'>
-            <header className='w-full h-14 bg-background-alternative absolute top-0 z-50'></header>
-            <Main />
+            <header className='absolute top-0 h-11 w-full bg-purple-900'></header>
+            <div className='h-full w-1/2 bg-violet-500 py-11'>
+                <div className='h-full w-full overflow-auto bg-slate-500 p-20'>
+                    <div className='bg-blue-500 w-full h-11'></div>
+                    <div className='grid auto-rows-[23rem] grid-cols-3 gap-3 p-5'>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                        <div className='h-full w-full bg-amber-300 transition-all hover:scale-125'></div>
+                    </div>
+                </div>
+                <footer className='h-11 w-full bg-orange-500'></footer>
+            </div>
         </div>
     );
 }
