43179cbe72d2c4e8fdb519f9284668600005c728
Mateo Ossa <trevorsoul.ts@gmail.com>
2023-01-03 17:21:05 -0300

    ahhh

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Main from './components/main/Main';
 
 function App(): JSX.Element {
     return (
-        <div className='w-screen h-screen flex justify-center bg-neutral-900 relative overflow-hidden'>
-            <header className='w-full h-14 bg-background-alternative absolute z-50'></header>
+        <div className='w-screen h-screen flex justify-center items-center bg-neutral-900 relative overflow-hidden'>
+            <header className='w-full h-14 bg-background-alternative absolute top-0 z-50'></header>
             <Main />
         </div>
     );
