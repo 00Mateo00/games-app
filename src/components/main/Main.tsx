63ba59e49427328e6da9c5e7f7a045b368650844
Mateo Ossa <trevorsoul.ts@gmail.com>
2023-01-04 01:52:02 -0300

    some progress in the mobile layout

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -25,15 +25,25 @@ const Main: React.FC<props> = ({ scrollRef }) => {
         );
     }
     return (
-        <div className='h-full w-1/2 bg-gradient-to-br from-violet-200 to-violet-900'>
+        <div
+            className={
+                'h-full w-full bg-gradient-to-br from-violet-200 to-violet-900' +
+                ' lg:w-[65rem]'
+            }
+        >
             <div
                 onWheel={(e) => e.stopPropagation()}
                 ref={scrollRef}
                 className='h-full w-full overflow-auto scrollbar:hidden'
             >
                 <Header />
-                <div className='mx-auto grid min-h-[calc(100%_-_92px)] w-[90%] auto-rows-[22rem] grid-cols-3 gap-5 py-2'>
-                    {getCards(12)}
+                <div
+                    className={
+                        'mx-auto grid min-h-[calc(100%_-_128px)] w-[90%] grid-flow-row auto-rows-[24rem] grid-cols-[repeat(auto-fit,_18rem)] place-content-center gap-5 py-2' +
+                        ' md:'
+                    }
+                >
+                    {getCards(13)}
                 </div>
                 <Footer />
             </div>
