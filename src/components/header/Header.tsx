805cfd762837ad88681fc559cefb8431ee07a4f4
mateo <mateo.ossa.b@gmail.com>
2024-10-13 15:02:50 -0300

    added link to portfolio, change some styles to the button

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import './header.scss';
 
 const Header: React.FC = () => {
     return (
         <header className='mx-auto flex h-20 w-[91%] items-center justify-between p-2'>
             <div className='h-fit'>
                 <button className='primary-button flex h-7 w-20 items-center justify-center rounded-xl'>
-                    HOME
+                    <a
+                        className='home'
+                        href='https://00mateo00.github.io/portfolio/'
+                    >
+                        HOME
+                    </a>
                 </button>
             </div>
             <div className='flex w-36 justify-between'>
